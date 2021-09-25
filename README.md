d72cecdc982ca7ab1aba82da9acc838b7ecb0964
poivronjaune <poivronjaune@gmail.com>
2021-09-24 21:39:29 -0400

    Setup README and REQUIREMENTS.TXT package details

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # Web Scraping on public web sites
+
+![GitHub](https://img.shields.io/github/license/MapleFrogStudio/AlgorithmicTrading?style=plastic)
+![GitHub last commit](https://img.shields.io/github/last-commit/MapleFrogStudio/AlgorithmicTrading?style=plastic)
+![PyPI - Python Version](https://img.shields.io/badge/python-3.4%2B-blue?color=blue&style=plastic)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/MapleFrogStudio/AlgorithmicTrading?logo=github&style=plastic)  
+  
+  
+Web scraping is a simple Selenium project to scrap data from a local provincial government web site.  
+
+##### Disclaimer:
+This repo is free to fork or download, it is a selenium and beautiful soup learning project. Make to sure to follow all usage conditions posted by the owner of the web site. 
+
+
+### 2021-09-24 Features
+- req.py contains python code to extract company information from the corresponding public web site (only returns data from the first page)  
+- This script controls a Chrome Browser on your local machine
+
+#### Upcomimg features
+- Loop though all pages of company information
+- Extract owners and managers from detailed information
+- Return data as a json object 
+
+#### Installation
+`git clone https://github.com/poivronjaune/web_scraping.git ` : Clone this repo to your workspace  
+` python -m venv env ` : Create a virtual environment  
+` env\Scripts\Activate ` : Activate the virtual environment (windows)  
+` python -m pip install --upgrade pip ` : Upgrade your pip tool  
+` pip install -r requirements.txt ` : Install python packages  
+` .env ` : Create a .env file and insert the following line ` CHROME_DRIVER_LOCATION = "C:\<path>\chromedriver.exe". Replace <path> with the location of your chromedriver.exe file
+
+The selenium package controls a web browser installed on your local machine. Please follow instructions on the pypi installation page : https://pypi.org/project/selenium/ to setup correctly. In a word, you need a special program called "chromedriver.exe" that will be accessible by your app. In the req.py file CHROME_DRIVER_LOCATION to the location of that file. The [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/downloads) program is produced and distributed by google.  
+
+
+#### Usage
+Once installation is complete, run ` python req.py <search_str>`. The script will:  
+- open a Chrome Web Browser
+- enter the <search_str> in the web site form
+- automatically submit the form
+- extract all company information found (first page only)  
+- display results on command line
+- wait for user to press ENTER then close the controlled browser
+
+
+# Packages
+#### Python packages
+` python-dotenv ` : pyhton package to manage environment variables     
+` selenium ` : pyhton package to extract data from web sites  
+` beautifulsoup4 ` : pyhton package to extract information from html web pages    
+` requests ` : pyhton package offering a simple python HTTP libray  
+` lxml ` : pyhton package offering XML processing library    
+
+
--- req.py
+++ req.py
@@ -2,26 +2,35 @@
 #
 #The next line can prevent use from using a global environmenet path variable set on the PC
 # System.setProperty("webdriver.chrome.driver","C:\\Users\\ghs6kor\\Desktop\\Java\\chromedriver.exe");
+import os
+import sys
+from dotenv import load_dotenv
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
 
-CHROME_DRIVER_LOCATION = "C:\Program Files (x86)\chromedriver.exe"
+# Load environment variables
+load_dotenv()
+
+#CHROME_DRIVER_LOCATION = "C:\<path>\chromedriver.exe"
+CHROME_DRIVER_LOCATION = os.environ['CHROME_DRIVER_LOCATION']
 url = "https://www.registreentreprises.gouv.qc.ca/RQAnonymeGR/GR/GR03/GR03A2_19A_PIU_RechEnt_PC/PageRechSimple.aspx?T1.CodeService=S00436&Clng=F&WT.co_f=2d06b7e3a15d2e0ab951627138812422"
-#string_to_search = "hitesh choudhary"
+
+company_to_search = "CHEZ ASHTON"  # Local fast food restaurant serving great poutine :)
+if len(sys.argv) > 1:
+    company_to_search = sys.argv[1]
 
 # Open the web page in chrome through python and shut up the error messages using options
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER_LOCATION,options=options)
-#driver = webdriver.Chrome(CHROME_DRIVER_LOCATION)
 driver.get(url)
 
 # Send a company name into the search box of the page
 search_box_xpath = '//*[@id="CPH_K1ZoneContenu1_Cadr_IdSectionRechSimple_IdSectionRechSimple_K1Fieldset1_ChampRecherche__cs"]'
 search_box = driver.find_element_by_xpath(search_box_xpath)
-company_to_search = "Fujitsu"
 search_box.send_keys(company_to_search)
 
 # Click the accept conditions check box
@@ -45,44 +54,21 @@ datagrid = driver.find_element_by_class_name("datagrid")
 companies = datagrid.find_elements_by_tag_name("tr")[1:]
 
 
-print(f"\n\n\n\n\n ------- DEBUG ------")
+print(f"\n\n\n\n\n ------- COMPANY INFORMATION ------")
 for company in companies:
     cells = company.find_elements_by_tag_name("td")
     print(f"neq: {cells[0].text}, statut:{cells[4].text}, nom:{cells[1].text}, adresse: {cells[3].text}")
 
 
 
-# print(driver)
-# print(datagrid.text)
-# print("-------------------------------------------------------")
-# print(len(companies))
-# print(companies[0].text)
-# print("-------------------------------------------------------")
 
 
 driver.minimize_window()
 #time.sleep(30)
-#driver.quit()
+input("Press ENTER to close the controlled Web Browser...")
+driver.quit()
 
 
 #############################################################################
-"""
-driver.get("http://url.com")    : Get a web page using a url
 
-driver.minimize()               : Minimizes the chrome browser window
-driver.quit()                   : quit the chorme driver and closes the web page
-driver.back()                   : Equivalent of pressing back button
-driver.forward()                : Go to page pressed when we pressed back button
-
-
-driver.find_element_by_name()   : Search the page using a name attribute
-driver.find_element_by_id()     : Search the page using an id attribute
-driver.clear()                  : clears the inout field before sending keys
-driver.sendkeys("some text")    : Send some text into a web component
-
-driver.title                    : Retrieve the web page title attribute
-driver.page_source              : Returns the entire source code of the page
-
-
-"""
 
