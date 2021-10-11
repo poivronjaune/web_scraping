732a7ea4079f00f2e9ee9ed0310e2ac80b53076d
poivronjaune <poivronjaune@gmail.com>
2021-10-11 07:41:44 -0400

    Fixed log errors to work with Jupyter

--- reqold.py
+++ reqold.py
@@ -23,17 +23,46 @@ class req:
         self.logger = None                      # Holder for the logging system
         self.data = None                        # Holder for company data
         self.search_term = None                 # Holder for search term
+        self.setup_logging(loglevel)
 
+        self.log_info(f"REQ Logging setup completed")
         self.setup_browser_driver()
+        if self.driver is None:
+            self.write_log_msg(f"REQ browser setup failed, check chromedriver version if compatible with your version of chrome")
+        else:
+            self.log_info(f"REQ browser setup succesfull")
 
 
     # ##################################################
     #  Utility methods for unse internally to class    #
     # ##################################################
-    def log_msg(self, msg):
-        # Central place to send feedback, TODO: implement a proper log mechanism
-        print(f"LOG: {msg}")
-    
+    def setup_logging(self, loglevel):
+        if loglevel is None:
+            return
+        else:
+            # https://docs.python.org/3/library/logging.html#logrecord-objects
+            ROOT_DIR = os.path.dirname(sys.modules['__main__'].__file__)
+            if not os.path.isdir(f"{ROOT_DIR}/LOGS"):
+                os.makedirs(f"{ROOT_DIR}/LOGS")
+            self.logger = logging.getLogger(__name__)
+            self.logger.setLevel(loglevel)
+            formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:[%(funcName)s(line:%(lineno)d)]:%(message)s")
+            file_handler = logging.FileHandler(f"{ROOT_DIR}/LOGS/{__name__}.log")
+            file_handler.setFormatter(formatter)
+            stream_handler = logging.StreamHandler()
+            stream_handler.setFormatter(formatter)
+            
+            self.logger.addHandler(file_handler)
+            self.logger.addHandler(stream_handler)
+
+    def log_info(self, info_str):
+        if self.logger is not None:
+            self.logger.info(info_str)
+
+    def log_debug(self, info_str):
+        if self.logger is not None:
+            self.logger.debug(info_str)
+
     def setup_browser_driver(self):
         #CHROME_DRIVER_LOCATION = "C:\<path>\chromedriver.exe"
         CHROME_DRIVER_LOCATION = os.environ["CHROME_DRIVER_LOCATION"]
@@ -41,15 +70,17 @@ class req:
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         try:
             self.driver = webdriver.Chrome(executable_path=CHROME_DRIVER_LOCATION,options=options)
+            self.log_debug(f"Chrome Browser opened and waiting for commands.")
         except Exception as e:
             self.driver = None
-            self.log_msg(f"Unable to open Chrome Browser, {e} ")
+            self.log_debug(f"Unable to open Chrome Browser, {e} ")
 
     def open_base_url(self):
         try:
             self.driver.get(self.BASE_URL)
+            self.log_info(f"REQ Base page opened, {self.BASE_URL}")
         except Exception as e:
-            self.log_msg(f"Unable to open base URL, {e}")
+            self.log_debug(f"Unable to open base URL, {e}")
 
     def send_search_string(self, company_to_search):
         self.search_term = company_to_search
@@ -67,7 +98,7 @@ class req:
         search_btn_xpath = '//*[@id="CPH_K1ZoneContenu1_Cadr_IdSectionRechSimple_IdSectionRechSimple_KRBTRechSimple_btnRechercher"]'
         search_btn = self.driver.find_element_by_xpath(search_btn_xpath)
         search_btn.click()        
-        self.log_msg(f'Company search completed, search="{company_to_search}"')
+        self.log_info(f'Company search completed, search="{company_to_search}"')
     
     def number_of_companies_found(self):
         total = 0
@@ -78,13 +109,13 @@ class req:
             if not str_end == 0:
                 total = found_number_str[0:str_end].strip()
                 total = int(total)
-                self.log_msg(f"Found company count in page, total={total}")
+                self.log_info(f"Found company count in page, total={total}")
             else:
                 total = 0
-                self.log_msg(f"No companies found")
+                self.log_info(f"No companies found")
         except Exception as e:
             total = 0
-            self.log_msg(f"Unable to locate 'dossier(s) trouvé(s) in search result page")
+            self.log_info(f"Unable to locate 'dossier(s) trouvé(s) in search result page")
 
         return total
 
@@ -104,7 +135,7 @@ class req:
         #
         if total_found > 0 and total_found <= 10 :
             data = self.extract_companies_from_html()
-            self.log_msg(f"One page of DATA extracted from REQ Site, total records: {total_found}")
+            self.log_info(f"One page of DATA extracted from REQ Site, total records: {total_found}")
         elif total_found > 10:
             total_pages = math.ceil(total_found / 10)
             # Automatically extract page 1, then proceed by loop for all other pages (all page links are not displayed in navigation tool bar)
@@ -113,10 +144,10 @@ class req:
                 self.select_results_page(page_num)
                 page_data = self.extract_companies_from_html()
                 data.extend(page_data)
-            self.log_msg(f"{total_pages} pages of DATA extracted from REQ Site, total records: {total_found}")
+            self.log_info(f"{total_pages} pages of DATA extracted from REQ Site, total records: {total_found}")
         else:
             data = []
-            self.log_msg(f"No DATA extracted from REQ Site, total records: 0")
+            self.log_info(f"No DATA extracted from REQ Site, total records: 0")
 
         self.data = data
         return data
@@ -154,9 +185,9 @@ class req:
             file_name = f"{filename}.csv"
             df = pd.DataFrame(self.data)
             df.to_csv("DATA/"+file_name, encoding='utf-8', index=False)
-            self.log_msg(f"Data saved to file: {file_name}")
+            self.log_info(f"Data saved to file: {file_name}")
         else:
-            self.log_msg(f"No Data saved to file.")
+            self.log_info(f"No Data saved to file.")
 
     def save_results_to_json(self, filename):
         if self.data != None:
@@ -164,9 +195,9 @@ class req:
             file_name = f"{filename}.json"
             df = pd.DataFrame(self.data)
             df.to_json("DATA/"+file_name, orient='records', force_ascii=False, indent=2)
-            self.log_msg(f"Data saved to file: {file_name}")
+            self.log_info(f"Data saved to file: {file_name}")
         else:
-            self.log_msg(f"No Data saved to file.")
+            self.log_info(f"No Data saved to file.")
 
     def save_results_to_excel(self, filename):
         if self.data != None:
@@ -174,7 +205,7 @@ class req:
             file_name = f"{filename}.xlsx"
             df = pd.DataFrame(self.data)
             df.to_excel("DATA/"+file_name, encoding='utf-8')
-            self.log_msg(f"Data saved to file: {file_name}")
+            self.log_info(f"Data saved to file: {file_name}")
         else:
-            self.log_msg(f"No Data saved to file.")
+            self.log_info(f"No Data saved to file.")
          
